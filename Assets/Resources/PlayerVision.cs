641361da3928c2e4ff663c23cb82a91eb5d50d2b
Devram <127593311+Ramera432@users.noreply.github.com>
2026-05-10 07:43:58 +1000

    Revert "OpeningScene"
    This reverts commit fc5951629fc46708e2c4f1880e379c15da6eda2c.

--- Assets/Resources/PlayerVision.cs
+++ Assets/Resources/PlayerVision.cs
@@ -20,8 +20,6 @@ public class PlayerVision : MonoBehaviour
     [Header("Rendering")]
     [SerializeField] private Material shadowMaterial;
     [SerializeField] private int sortingOrder = 500;
-    [SerializeField] private float wallRevealPadding = 0.35f;
-    [SerializeField] private float minimumHitDistance = 0.2f;
 
     [Header("Layer")]
     public LayerMask obstacleMask;
@@ -216,29 +214,10 @@ public class PlayerVision : MonoBehaviour
     private VisionRay CastVisionRay(Vector2 direction)
     {
         Vector2 origin = transform.position;
-
-        RaycastHit2D hit = Physics2D.Raycast(
-            origin,
-            direction,
-            viewRadius,
-            obstacleMask
-        );
-
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, viewRadius, obstacleMask);
         if (hit.collider != null)
         {
-            float hitDistance = hit.distance;
-
-            // If the ray hits basically immediately, don't let it black out everything.
-            if (hitDistance < minimumHitDistance)
-            {
-                hitDistance = minimumHitDistance;
-            }
-
-            // Make the darkness start slightly after the wall hit.
-            // This lets you actually see the wall surface near you.
-            float shadowStart = Mathf.Min(hitDistance + wallRevealPadding, viewRadius);
-
-            return new VisionRay(shadowStart, shadowStart);
+            return new VisionRay(hit.distance, hit.distance);
         }
 
         float fadeStartDistance = Mathf.Max(0f, viewRadius - edgeFadeDistance);
